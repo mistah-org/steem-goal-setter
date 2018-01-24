ec0ddcede0c2f491355b21993f7da0520a85e0f2
East <eastmaels@gmail.com>
2018-01-24 14:58:51 +0800

    added controller method to receive start and end dates

--- src/main/java/org/mistah/steemgoalsetter/controller/FollowersController.java
+++ src/main/java/org/mistah/steemgoalsetter/controller/FollowersController.java
@@ -1,39 +1,54 @@
 package org.mistah.steemgoalsetter.controller;
 
-import javax.inject.Inject;
+import java.util.Date;
 
-import org.mistah.steemgoalsetter.service.BookService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.MessageSource;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class FollowersController {
 
     final Logger logger = LoggerFactory.getLogger(FollowersController.class);
 
-    @Inject
-    private BookService bookService;
-
-    @Inject
-    private MessageSource messageSource;
+    @InitBinder
+    public void initBinder(WebDataBinder binder) {
+        // true passed to CustomDateEditor constructor means convert empty String to null
+        binder.registerCustomEditor(Date.class, new CustomDateEditor(new MultiDateFormat(), true));
+    }
 
     /**
      * Retrieve the book with the specified id.
      */
     @RequestMapping(value = "/{username}/followers", method = RequestMethod.GET)
-    public String followers(@PathVariable("username") String username, Model model) {
-        logger.info("Username: " + username);
+    public String show(
+    		@PathVariable("username") String username,
+    		Model model) {
+        return "account/followers";
+    }
 
-//        Book book = bookService.findById(id);
-//        model.addAttribute("book", book);
+    /**
+     * Retrieve the book with the specified id.
+     */
+    @RequestMapping(value = "/{username}/followers", method = RequestMethod.POST)
+    public String update(
+    		@PathVariable("username") String username,
+    		@RequestParam(value="startDate", required=false) Date fromDate,
+    		@RequestParam(value="endDate", required=false) Date toDate,
+    		Model model) {
+        logger.info("from: " + fromDate);
+        logger.info("to: " + toDate);
 
         return "account/followers";
     }
+
 
 }
